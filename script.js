8c269c0487c96b578a1f929c8a0275a3961bbe59
Amit_Valand <143027284+Amitvaland14@users.noreply.github.com>
2024-09-08 23:20:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-document.getElementById('next').onclick = function(){
+document.getElementById('next').onclick = function () {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').appendChild(lists[0]);
 }
-document.getElementById('prev').onclick = function(){
+
+document.getElementById('prev').onclick = function () {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
-let autoNext = setInterval(function() {
+
+let autoNext = setInterval(function () {
     document.getElementById('next').click();
 }, 3000);
 
-
-const audio = new Audio("./Song/Song1.mp3");
-document.onclick = ()=>{
-    audio.play()
-}
-  
+const audio = document.getElementById('background-music');
+document.addEventListener('DOMContentLoaded', () => {
+    audio.play().catch(() => {
+        document.addEventListener('click', () => {
+            audio.play();
+        }, { once: true });
+    });
+});
